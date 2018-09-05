c394520c59a71a55498dca76272d4ed5c4ba676c
MrVeshij <35579329+MrVeshij@users.noreply.github.com>
2018-09-05 22:44:24 +0300

    first commit

---
+++ django_21/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\veshi\desktop\my_study\site_card\django_21\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
